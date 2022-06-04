ebc4e6384d5f93d36ad617ed54137605fedfa557
Jon Trauntvein <jon@campbellsci.com>
2022-06-04 12:30:27 -0600

    chore: added jsdoc comments for the msbuild target generator.

--- src/MsBuild.js
+++ src/MsBuild.js
@@ -1,12 +1,27 @@
 const Execute = require("./Execute");
 
-
+/**
+ * @typedef MsBuildOptionsType
+ * @property {string} name Specifies the target name
+ * @property {Array<string> = []} depends Specifies the target names upon which the generated target will depend.
+ * @property {Object={}} options Specifies target options.  If the makefile.js exported function is passed an options
+ * parameter, that parameter must be passed for this value.
+ * @property {string} project_file Specifies the name of the project file
+ * @property {string?} cwd Specifies the directory from which the project should be built.
+ * @property {string?} platform Specifies the platform identifier ("x86" or "x64").  If not specified, the default platform will be used.
+ * @property {string="Release"} config Specifies the build configuration to be built.
+ */
+/**
+ * @description Generates a target that will invoke the visual studio build tool (msbuild) with a specified configuration and optional
+ * platform.
+ * @param {MsBuildOptionsType} options Specifies the options for the target.
+ */
 async function make_msbuild({
    name,
    depends = [],
    options = {},
    project_file,
-   cwd,
+   cwd = undefined,
    platform = undefined,
    config = "Release"
 }) {
@@ -16,7 +31,7 @@ async function make_msbuild({
    ];
    if(platform)
       msbuild_args.push(`/p:Platform=${platform}`);
-   Execute.execute({
+   return Execute.execute({
       name,
       depends,
       ignore_exit_code: false,
