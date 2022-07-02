59ce156c9c79ae249d6b55a560919c5f816af8bb
Jon Trauntvein <jon@campbellsci.com>
2022-07-02 10:06:57 -0600

    feat: Added a target type to perform an HTTP request.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const MakeCDecl = require("./MakeCDecl");
 const Subdir = require("./Subdir");
 const PickDirTargets = require("./PickDirTargets");
 const MsBuild = require("./MsBuild");
+const HttpRequest = require("./HttpRequest");
 
 
 module.exports = {
@@ -13,6 +14,7 @@ module.exports = {
    make_cdecl: MakeCDecl.make_cdecl,
    subdir: Subdir.subdir,
    pick_dir_targets: PickDirTargets.pick_dir_targets,
+   http_request: HttpRequest.http_request,
    msbuild: MsBuild.msbuild
 };
 
