d315214c8df2f5edaee80c750eb22ceb65909a80
Jon Trauntvein <jon@campbellsci.com>
2022-07-15 07:54:16 -0600

    fix: refactored the rsync() function so that it uses its own algorithm rather than rsyncjs.

--- makefile.js
+++ makefile.js
@@ -1,5 +1,17 @@
 const Subdir = require("./src/Subdir");
+const RSync = require("./src/RSync");
+const os = require("node:os");
+const path = require("node:path");
+
 
 module.exports = async function(options) {
    await Subdir.subdir({ name: "src", options });   
+   await RSync.rsync({
+      name: "copy-to-temp",
+      source: "src",
+      dest: path.join(os.homedir(), "temp", "src"),
+      delete_orphaned: true,
+      filter: (file_name) => { return !file_name.endsWith(".h") },
+      options
+   })
 };
--- src/RSync.js
+++ src/RSync.js
@@ -1,23 +1,149 @@
 const Target = require("./Target");
-const rsyncjs = require("rsyncjs");
+const fs = require("node:fs/promises");
+const path = require("node:path");
+let sync_dir;
+const file_types = {
+   directory: 0,
+   file: 1,
+   other: 2
+};
+const file_ops = {
+   copy: async function(op_desc) {
+      await fs.copyFile(op_desc.source, op_desc.dest);
+      await fs.utimes(op_desc.dest, new Date(op_desc.atime), new Date(op_desc.mtime));
+   },
+   rm: async function(op_desc) {
+      await fs.rm(op_desc.path, { force: true, recursive: true });
+   },
+   sync: async function(op_desc) {
+      await sync_dir(op_desc.target, op_desc.source, op_desc.dest, op_desc.target.filter, op_desc.target.delete_orphaned);
+   }
+};
+
+sync_dir = function sync_dir(target, source, dest, filter, delete_orphaned) {
+   return new Promise((accept, reject) => {
+      try {
+         // we need to ensure that the destination directory exists.
+         fs.mkdir(dest, { recursive: true }).then(() => {
+            // we need to read the source directory
+            fs.readdir(source).then((items) => {
+               // we need to consider whether the each source item passes the filter
+               const source_items = items.filter((item) => {
+                  const source_path = path.join(source, item);
+                  let rtn = true;
+                  if(typeof filter === "function")
+                     rtn = filter(source_path);
+                  else if(typeof filter === "object" && filter instanceof RegExp)
+                     rtn = source_path.match(filter);
+                  return rtn;
+               });
+               fs.readdir(dest, { withFileTypes: true }).then((dest_items) => {
+                  // we need to get the stats for each of the source and dest items.  Once we have those, we can 
+                  // determine what, if any operations need to be done on each.
+                  const source_stat_promises = source_items.map((file_name) => {
+                     return new Promise((stat_accept, stat_reject) => {
+                        fs.stat(path.join(source, file_name)).then((stat) => {
+                           stat.name = file_name;
+                           stat_accept(stat);
+                        }).catch((error) => {
+                           stat_reject(error);
+                        });
+                     });
+                  });
+                  Promise.allSettled(source_stat_promises).then((source_stat_results) => {
+                     const source_stats = source_stat_results.map((result) => result.value);
+                     const dest_stat_promises = dest_items.map((dest_item) => {
+                        return new Promise((stat_accept, stat_reject) => {
+                           fs.stat(path.join(dest, dest_item.name)).then((stat) => {
+                              stat.name = dest_item.name;
+                              stat_accept(stat);
+                           }).catch((stat_error) => {
+                              stat_reject(stat_error);
+                           });
+                        });
+                     });
+                     Promise.allSettled(dest_stat_promises).then((dest_results) => {
+                        // we need to generate the list of operations that will need to be performed.
+                        const ops = [];
+                        const dest_stats = dest_results.map((result) => result.value);
+                        source_stats.forEach((source_stat) => {
+                           const matching_dest = dest_stats.find((dest_stat) => dest_stat.name.toUpperCase() === source_stat.name.toUpperCase());
+                           const source_path = path.join(source, source_stat.name);
+                           const dest_path = path.join(dest, source_stat.name);
+                           if(matching_dest !== undefined)
+                           {
+                              const source_type = (source_stat.isDirectory() ? file_types.directory : (source_stat.isFile() ? file_types.file : file_types.other));
+                              const dest_type = (matching_dest.isDirectory() ? file_types.directory : (matching_dest.isFile() ? file_types.file : file_types.other));
+                              if(source_type === dest_type && source_type === file_types.file) 
+                              {
+                                 if(source_stat.mtime.toString() !== matching_dest.mtime.toString())
+                                    ops.push({ op: "copy", source: source_path, dest: dest_path, ctime: source_stat.ctime, mtime: source_stat.mtime });
+                              }
+                              if(source_type === dest_type && source_type === 0)
+                                 ops.push({ op: "sync", source: source_path, dest: dest_path });
+                              if(source_type !== dest_type)
+                              {
+                                 // we will first add an op to delete the directory and will then add an op 
+                                 ops.push({ op: "rm", path: dest_path });
+                                 if(source_type === 0)
+                                    ops.push({ op: "sync", source: source_path, dest: dest_path });
+                                 if(source_type === 1)
+                                    ops.push({ op: "copy", source: source_path, dest: dest_path });
+                              }
+                           }
+                           else
+                           {
+                              if(source_stat.isDirectory())
+                                 ops.push({ op: "sync", source: source_path, dest: dest_path });
+                              else if(source_stat.isFile())
+                                 ops.push({ op: "copy", source: source_path, dest: dest_path, mtime: source_stat.mtimeMs, ctime: source_stat.ctimeMs });
+                           }
+                        });
+
+                        // if the delete orphaned flag is set, we will need to delete any dest items that are not in the source directory.
+                        if(delete_orphaned)
+                        {
+                           dest_items.forEach((dest_item) => {
+                              const source_item = source_items.find((source_item) => source_item.toUpperCase() === dest_item.name.toUpperCase());
+                              if(!source_item)
+                                 ops.push({ op: "rm", path: path.join(dest, dest_item.name) });
+                           });
+                        }
+                        target.pending_ops.push(...ops);
+                        accept();
+                     }).catch((error) => {
+                        reject(error);
+                     });
+                  }).catch((error) => {
+                     reject(error);
+                  });
+               }).catch((error) => {
+                  reject(error);
+               });
+            }).catch((error) => {
+               reject(error);
+            })
+         }).catch((error) => {
+            reject(error);
+         });
+      }
+      catch(error) {
+         reject(error);
+      }
+   });
+}
+
 
 async function do_rsync() {
-   return new Promise((accept, reject) => {
-      const async = rsyncjs.async;
-      async(this.source, this.dest, {
-         deleteOrphaned: this.delete_orphaned,
-         exclude: this.exclude,
-         filter: this.filter,
-         onError: function(error) {
-            this.reported_error = error;
-         }
-      }).then(() => {
-         if(this.reported_error)
-            reject(this.reported_error);
-         else
-            accept(true);
-      });
-   });
+   this.pending_ops = [];
+   await sync_dir(this, this.source, this.dest, this.filter, this.delete_orphaned);
+   while(this.pending_ops.length > 0)
+   {
+      const op = this.pending_ops[0];
+      this.pending_ops.splice(0, 1);
+      await file_ops[op.op](op);
+   }
+   return true;
 }
 
 
