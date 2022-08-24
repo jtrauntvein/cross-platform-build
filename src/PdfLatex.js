cf2846b7af48ecbd72b20f4a7a8019858d290ce1
Jon Trauntvein <jon@campbellsci.com>
2022-08-24 08:46:15 -0600

    fix: fixed another bug in recently introduced code in pdf_latex()

--- src/PdfLatex.js
+++ src/PdfLatex.js
@@ -65,7 +65,7 @@ async function do_pdf_latex(document, defines, quiet, once, options, check_outpu
       iterate_results = await iterate();   
       complete = (iterate_results.error_count > 0 ||  once || iterate_results.rerun_count === 0);
    }
-   return (iterate_results.error_count === 0);
+   return (output_checks || iterate_results.error_count === 0);
 }
 
 
